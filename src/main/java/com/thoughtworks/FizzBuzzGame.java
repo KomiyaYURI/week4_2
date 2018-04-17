10f04eca1f27a603e46bf8c3873dbb9814aad312
KomiyaYURI <879513168@qq.com>
2018-04-17 17:13:39 +0800

    a

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -8,11 +8,40 @@ public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
 
     public void start(int count) {
+        boolean special;
+        StringBuilder personalResult = new StringBuilder();
 
+        for (int i = 1; i <= 100; i++)
+        {
+            special = false;
+            personalResult.setLength(0);
+
+            if (i % 3 == 0) {
+                special = true;
+                personalResult.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                special = true;
+                personalResult.append("Buzz");
+            }
+            if (i % 7 == 0) {
+                special = true;
+                personalResult.append("Whizz");
+            }
+
+            if (!special)
+                personalResult.append(Integer.toString(i));
+
+            results.add(personalResult.toString());
+        }
     }
 
     public List<String> getResults() {
         return results;
+    }
+
+    public String parseNumberToWord(int i) {
+        return results.get(i-1);
     }
 }
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,46 @@
 package com.thoughtworks;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class FizzBuzzGameTest {
+    private FizzBuzzGame game;
+
+    @Before
+    public void setUp() {
+        game = new FizzBuzzGame();
+        game.start(100);
+    }
+
+    @Test
+    public void should_return_1_when_have_1() {
+        String word = game.parseNumberToWord(1);
+        assertEquals("1", word);
+    }
+
+    @Test
+    public void should_return_Fizz_when_have_4() {
+        String word = game.parseNumberToWord(4);
+        assertEquals("4", word);
+    }
+
     @Test
     public void should_return_Fizz_when_have_3() {
+        String word = game.parseNumberToWord(3);
+        assertEquals("Fizz", word);
+    }
 
+    @Test
+    public void should_return_Fizz_when_have_3_multiplier_like_9() {
+        String word = game.parseNumberToWord(9);
+        assertEquals("Fizz", word);
+    }
+
+    @Test
+    public void should_return_FizzBuzz_when_have_3_and_5_multiplier_like_15() {
+        String word = game.parseNumberToWord(15);
+        assertEquals("FizzBuzz", word);
     }
 }
